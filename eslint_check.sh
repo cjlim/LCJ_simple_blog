2983790467232a365dc2565dd403c3be525f280f
limchaejoo <shine5311@adain.kr>
2016-04-15 22:37:22 +0900

    start

---
+++ eslint_check.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+eslint --ignore-path .eslintignore --fix --cache --quiet project-c-service/**/*.js
---
+++ simple_blog/client/views/main.js
@@ -0,0 +1,18 @@
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter: function() {
+    return Template.instance().counter.get();
+  }
+});
+
+Template.hello.events({
+  'click button': function(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  }
+});
