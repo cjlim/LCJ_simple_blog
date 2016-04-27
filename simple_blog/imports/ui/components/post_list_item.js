d543f480e5d66f6bd833fbc740418e4e3381ed9d
limchaejoo <shine5311@adain.kr>
2016-04-28 02:59:41 +0900

    이미지 등록시 리스트에 이미지 섬네일 나오도록 수정

--- simple_blog/imports/ui/components/post_list_item.js
+++ simple_blog/imports/ui/components/post_list_item.js
@@ -1,14 +1,32 @@
 
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './post_list_item.html';
 
 Template.postListItem.onCreated(function() {
+  this.imgSrc = new ReactiveDict();
+  this.pText = new ReactiveDict();
 });
 
 Template.postListItem.onRendered(function() {
+  const self = this;
   const data = this.data;
-  console.log(data);
+  let imgIndex = data.content.indexOf("<img");
+
+  // content에서 img가 있는지 체크
+  // 있으면 이미지의 src 값을 가져옴.
+  // src 값을 넣고 helper로 넣어줌.
+  if(imgIndex !== -1){
+    let imgSrc = data.content.slice(imgIndex);
+
+    imgIndex = imgSrc.indexOf("src");
+    imgSrc = imgSrc.slice(imgIndex + 5);
+    imgIndex = imgSrc.indexOf('"');
+    imgSrc = imgSrc.substring(0, imgIndex);
+
+    self.imgSrc.set("postImgSrc", imgSrc);
+  }
 });
 
 Template.postListItem.helpers({
@@ -19,7 +37,10 @@ Template.postListItem.helpers({
     var month = date.getMonth() + 1;
     var day = date.getDate();
 
-    return month + '. ' + day + '. ' + year;
+    return year + '. ' + month + '. ' + day;
+  },
+  hasSrc: function(){
+    return Template.instance().imgSrc.get("postImgSrc");
   }
 });
 
